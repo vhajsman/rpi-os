40543d0a670ce6b416105c165f95ccda2154760c
vhajsman <vaclav.hajsman@hotmail.com>
2024-02-19 19:41:13 +0100

    Initialize memory at boot

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,15 +1,17 @@
 #include <stdint.h>
 #include "drivers/serial/uart.h"
+#include "kernel/memory/memory.h"
 
 #define UNUSED(X)   (void) X
 
 void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
     UNUSED(r0);
     UNUSED(r1);
-    UNUSED(atags)
+    
+    // Initialize the memory.
+    memory_init(atags);
 
     uart_init();
-    uart_puts("Hello, kernel World!\r\n");
 
     while (1) {
         uart_putc(uart_getc());
